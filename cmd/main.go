b5372f4ee1440823e5bfde162d303f66dfdf2920
akhilesh-dev <akhilesh@yajtechnologies.com>
2025-11-25 15:21:12 +0545

    render

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
-	"tiny-drop/internal/routes"
-	"tiny-drop/internal/templates"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+var templates *template.Template
+
 func main() {
+	templates = template.Must(template.ParseGlob("web/templates/*.html"))
+	log.Printf("Parsed templates: %v", templates.DefinedTemplates())
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	
-	fileServer := http.FileServer(http.Dir("./web/static"))
-	r.Handle("/static/*", http.StripPrefix("/static/", fileServer))
-	
-	renderer := templates.NewRenderer("./web/templates", true)
-	
-	
-	routes.ApiRoutes(r, renderer)
-	
-	port := ":3000"
-	log.Println("Listening on ", port)
-	http.ListenAndServe(port, r)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "home", nil)
+	})
+
+	r.Get("/about", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "about", nil)
+	})
+
+	r.Get("/contact", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "contact", nil)
+	})
+
+	log.Println("Server running at http://localhost:9090")
+
+	// THIS IS THE MOST IMPORTANT LINE
+	err := http.ListenAndServe(":9090", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func render(w http.ResponseWriter, page string, data interface{}) {
+	err := templates.ExecuteTemplate(w, page, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
